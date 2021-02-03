7fa52754c3c5bb8bc0e600151bd2447042b269c2
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-02-03 00:21:15 -0800

    final

---
+++ PythagoreanTheorem/src/PythagoreanTheoremTest.java
@@ -0,0 +1,9 @@
+public class PythagoreanTheoremTest{
+	public static void main(String[] args) {
+		PythagoreanTheorem test = new PythagoreanTheorem();
+//		double hypotenuse = test.calculateHypotenuse(3, 3);
+		double hypotenuse = test.calculateHypotenuse(6, 5);
+		System.out.println(hypotenuse);
+		
+	}
+}
