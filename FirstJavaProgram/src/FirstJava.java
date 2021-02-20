90324d9dfae8d249ccdb741a1afdae0a659e865a
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-02-20 00:32:33 -0800

    update

--- FirstJavaProgram/src/FirstJava.java
+++
@@ -1,7 +0,0 @@
-public class FirstJava {
-	public static void main (String [] args) {
-		System.out.println("My name is Michael");
-		System.out.println("I am 100 years old");
-		System.out.println("I live in North Hollywood");
-	}
-}
